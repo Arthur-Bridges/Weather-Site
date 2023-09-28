2af75c98203680681f5e57d97f3f488ea18a5aa4
Arthur Bridges <killor@Arthurs-MBP.lan>
2023-09-28 00:49:34 -0500

    Adding files with skeleton code and APIs with their syntax

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# Weather-Site
-Site to see the weather forcast
+#Weather App
+
+##Description
+
+A webpage that allows you to get the forcast anywhere in the world
+
+##Installation
+
+N/A
+
+##Usage
+Deployed website is here:
+
+N/A
+
+##License 
+
+Credit to the author of this repo.
---
+++ assets/script.js
@@ -0,0 +1,12 @@
+/** API's and 
+ 1)WeatherApi = api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+
+ 2)GeoLocation Api = http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+
+ SYNTAX
+
+ Weather = https://openweathermap.org/forecast5#geo5
+
+ GeoLocation = https://openweathermap.org/api/geocoding-api
+ */
+
