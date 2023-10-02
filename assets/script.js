01c827b6057cd94ca3af426dc59c4e058480ff25
Arthur Bridges <killor@Arthurs-MBP.lan>
2023-10-02 16:23:58 -0500

    still debugging trying to get the objects to show

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,7 @@ $(document).ready(function (){
 
 });
 
-function saveHistory(){
+function saveHistory(cityName){
     function displayHistory(){
         var historyL = $("#historyList");
         for (var i = 0; i < historyList.length; i++){
@@ -20,7 +20,7 @@ function saveHistory(){
     displayHistory();
 }
 
-function getWeatherData (){
+    function getWeatherData (){
     //TODO: add container for city is searched and inserted within the url
     //getting Lat and Lon using GeocodeAPI
     function search(){
@@ -34,23 +34,25 @@ function getWeatherData (){
     }
     
     function getLocation(cityName){
-    var cityUrl =  "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName +"&limit=5&appid=9dc3e6bd849515c45ff7f316e0a2987e";
+    var cityUrl =  "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName +"&limit=1&appid=9dc3e6bd849515c45ff7f316e0a2987e";
     fetch(cityUrl).then(function (response) {
     return response.json();
     }).then(function (data){
     var lat = data[0].lat;
     var lon = data[0].lon;
+    console.log(data);
     getWeather(lat, lon);
     });
 }
 //getting Weather Data
-    function getWeather(lat, lon){
+    async function getWeather(lat, lon){
     var requestUrl = "http://api.openweathermap.org/data/2.5/forecast?lat="+ lat +"&lon="+ lon +"&appid=9dc3e6bd849515c45ff7f316e0a2987e&units=imperial";
     fetch(requestUrl).then(function (response) {
         return response.json();
     }).then(function (data){
         console.log(data);
-        var content = data;
+        var content = data.list;
+        
         displayContent(content);
     });
 }
@@ -71,29 +73,45 @@ function getWeatherData (){
     */
    //Might not iterate through columns
    //may use .html instead of .text
+   //issue with function below
    function displayContent(content) {
     var col = $(".column");
-    for (var i = 0; i < col.length; i++){
-        var listCityName = $("<li>").text("City: " + content[i].name);//CITY NAME is data.city.name
-        var listWeatherIcon = $("<li>").text(content[i].weather[0].icon);// WEATHER CONDITION ICON might be data.list.main.weather[i].icon or data.main.weather[i].icon
-        var listWeatherCondition = $("<li>").text("Weather Condition " + content[i].weather[0].description);// WEATHER CONDITION DESCRIPTION might be data.list.main.weather[i].description or data.main.weather[i].description
-        var listTemp = $("<li>").text("Temperature: " + Math.round(content[i].main.temp));// TEMP which is either data.list.main.temp or data.main.temp
-        var listHumidity = $("<li>").text("Humidity: " + content[i].main.humidity + "°");// HUMIDITY which is either data.list.main.humidity or data.main.humidity
+    var nameI = $("#cityName");
+    var listCityName = $("<li>").html("City: " + content.name +"\n");//CITY NAME is data.city.name
+    nameI.append(listCityName);
+    for(var i = 0; i < col.length && i < content.length; i++){
+        
+        var listWeatherIcon = $("<li>").text(content[i].weather[0].icon +"\n");// WEATHER CONDITION ICON might be data.list.main.weather[i].icon or data.main.weather[i].icon
+        var listWeatherCondition = $("<li>").text("Weather Condition " + content[i].weather[0].description +"\n");// WEATHER CONDITION DESCRIPTION might be data.list.main.weather[i].description or data.main.weather[i].description
+        var listTemp = $("<li>").text("Temperature: " + Math.round(content[i].main.temp)+ "\n");// TEMP which is either data.list.main.temp or data.main.temp
+        var listHumidity = $("<li>").text("Humidity: " + content[i].main.humidity + "°\n");// HUMIDITY which is either data.list.main.humidity or data.main.humidity
         var listWindSpeed = $("<li>").text("Wind Speed: " + content[i].wind.speed + "mph");// WIND SPEED which is either data.list.wind.speed or data.wind.speed
-
-        col[i].append(listCityName);
         col[i].append(listWeatherIcon);
         col[i].append(listWeatherCondition);
         col[i].append(listTemp);
         col[i].append(listHumidity);
-        col[i].append(listWindSpeed);
+        
+        col[i].append(listWindSpeed);        
     }
+    // for (var i = 0; i < col.length; i++){
+
+    //     var listCityName = $("<li>").text("City: " + content.name +"\n");//CITY NAME is data.city.name
+    //     var listWeatherIcon = $("<li>").text(content.weather[0].icon +"\n");// WEATHER CONDITION ICON might be data.list.main.weather[i].icon or data.main.weather[i].icon
+    //     var listWeatherCondition = $("<li>").text("Weather Condition " + content.weather.description +"\n");// WEATHER CONDITION DESCRIPTION might be data.list.main.weather[i].description or data.main.weather[i].description
+    //     var listTemp = $("<li>").text("Temperature: " + Math.round(content.main.temp)+ "\n");// TEMP which is either data.list.main.temp or data.main.temp
+    //     var listHumidity = $("<li>").text("Humidity: " + content.main.humidity + "°\n");// HUMIDITY which is either data.list.main.humidity or data.main.humidity
+    //     var listWindSpeed = $("<li>").text("Wind Speed: " + content.wind.speed + "mph");// WIND SPEED which is either data.list.wind.speed or data.wind.speed
+
+    //     col[i].append(listCityName);
+    //     col[i].append(listWeatherIcon);
+    //     col[i].append(listWeatherCondition);
+    //     col[i].append(listTemp);
+    //     col[i].append(listHumidity);
+    //     col[i].append(listWindSpeed);
+    // }
 }
 
 search();
-getLocation();
-getWeather();
-displayContent();
 saveHistory();
 }
 
